5d985e9d6b90b25c25f10f37360d923925e2b8a7
Nehal Singh <nehalsingh@Nehals-MacBook-Pro.local>
2019-09-23 04:40:56 -0500

    Mini project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion'
+import pogo from '/Users/nehalsingh/Applications/hackillinoisinfo/src/hack Illinois Image.png';
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  constructor() {
+    super()
+    this.state ={
+      data:[]
+    }
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link">
+            
+            <img src={pogo} alt="Logo"/>
+           Schedule
+          </a>
+
+        </header>
+        {this.state.data.map(event => (
+          <div>
+            <p className = "Events">{event.name}</p>
+            <p className = "Description">{event.description}</p>
+            <p className = "Time">Time: {event.startTime} - {event.endTime}</p>
+            <p className = "locations">Venue - {event.locations[0].description} ( {event.locations[0].latitude} , {event.locations[0].longitude})</p>
+            <p className = "sponsor">Sponsored by {event.sponsor}</p>
+            <p className = "eventType">{event.eventType}</p>
+              
+            
+
+            </div>
+        ))}
+      </div>
+    );
+} 
+  //make a fetch request
+  getEvents() {
+      //put fetch request here
+      fetch('https://api.hackillinois.org/event/', {
+        method : "GET",
+        headers: {
+          'Content-Type' : 'application/json',
+        }
+      })
+      .then((response) =>response.json())
+      .then((responseJson) => {
+        console.log(responseJson.events)
+        this.setState({data:responseJson.events});
+      })
+
+
+  }
+
+   componentDidMount() {
+    this.getEvents();
+  }
+
+  
+
+  }
+  export default App;
